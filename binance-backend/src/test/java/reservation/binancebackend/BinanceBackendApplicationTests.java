da0704e4a5e8d1d448da2ae110bd304e10e97c2d
Yun <mosy2266@naver.com>
2026-08-14 18:17:21 +0900

    Backend Initial Commit

---
+++ binance-backend/src/test/java/reservation/binancebackend/BinanceBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package reservation.binancebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BinanceBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
